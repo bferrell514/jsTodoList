a64b9a76699491073d0e78cce55a72389ef9d7a5
bferrell514 <bferrell514@gmail.com>
2019-03-28 17:59:39 -0400

    Update to Version 2
    display, add, change and delete functions for my Todo List

--- functions.js
+++ functions.js
@@ -1,1 +1,22 @@
 //functions below
+
+var todos = ['item 1', 'item 2', 'item 3', 'item 4b']
+
+function displayTodos() {
+	console.log('My todos:', todos);
+    }
+    
+function addTodo(todos) {
+	todos.push(todos);
+	displayTodos()
+    }
+    
+function changeTodo(position, newValue) {
+    todos[position] = newValue;
+	displayTodos();
+}
+
+function deleteTodo(position) {
+    todos.splice(position, 1)
+    displayTodos();
+}
