e0c66631303c84fc60097436b2e9553b6e56b7d7
bferrell514 <bferrell514@gmail.com>
2019-03-29 00:47:07 -0400

    loops (version 5)

---
+++ loops.js
@@ -0,0 +1,32 @@
+
+var todoList = {
+    todos: [], 
+    displayTodos: function() {
+        if (this.todos.length === 0){
+            console.log('Your todo list is empty!')
+        } else {
+            for (var i = 0; i < this.todos.length; i++) {
+            
+            if (this.todos[i].completed === true) {
+                console.log('(x)', this.todos[i].todoText);
+            } else {
+                console.log('()', this.todos[i].todoText);
+            }
+        }
+    }
+},
+    addTodo: function(todoText) {
+        this.todos.push({
+           todoText: todoText,
+           completed: false
+        });
+    },
+    changeTodo: function(position, newValue) {
+        this.todos[position] = newValue;
+        this.displayTodos();
+    },
+    deleteTodo: function(position) {
+        this.todos.splice(position, 1)
+        this.displayTodos();
+    }
+};
