d7af07eb46cd9ce560bea6abcb6efe188f82d6fa
bferrell514 <bferrell514@gmail.com>
2019-03-30 12:06:10 -0400

    adding objects to my js file

--- todoList.js
+++ todoList.js
@@ -1,25 +1,20 @@
-var todoList = {
-    todos: [], 
-    displayTodos: function() {
-        console.log('My Todos', this.todos)
-    },
-    addTodo: function(todoText) {
-        this.todos.push({
-            todoText: todoText,
-            completed: false,
-    });
-    this.displayTodos()
-    },
-    changeTodo: function(position, todoText) {
-        this.todos[position].todoText = todoText;
-        this.displayTodos();
-    },
-    deleteTodo: function(position) {
-        this.todos.splice(position, 1)
-        this.displayTodos();
-    },
-    toggleCompleted: function(position) {
-        var todo = this.todos[position];
-        todo.coompleted = !todo.completed;
-        this.displayTodos();
+var todos = ['item 1', 'item 2', 'item 3', 'item 4b']
+
+function displayTodos() {
+	console.log('My todos:', todos);
     }
+    
+function addTodo(todos) {
+	todos.push(todos);
+	displayTodos()
+    }
+    
+function changeTodo(position, newValue) {
+    todos[position] = newValue;
+	displayTodos();
+}
+
+function deleteTodo(position) {
+    todos.splice(position, 1)
+    displayTodos();
+}
