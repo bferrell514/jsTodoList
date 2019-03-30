d752bb0e2aeac5afb9e71b08c6b537e4bb8c69bb
bferrell514 <bferrell514@gmail.com>
2019-03-30 12:05:13 -0400

    adding booleans to javascript

--- todoList.js
+++ todoList.js
@@ -1,22 +1,25 @@
-//functions below
-
-var todos = ['item 1', 'item 2', 'item 3', 'item 4b']
-
-function displayTodos() {
-	console.log('My todos:', todos);
+var todoList = {
+    todos: [], 
+    displayTodos: function() {
+        console.log('My Todos', this.todos)
+    },
+    addTodo: function(todoText) {
+        this.todos.push({
+            todoText: todoText,
+            completed: false,
+    });
+    this.displayTodos()
+    },
+    changeTodo: function(position, todoText) {
+        this.todos[position].todoText = todoText;
+        this.displayTodos();
+    },
+    deleteTodo: function(position) {
+        this.todos.splice(position, 1)
+        this.displayTodos();
+    },
+    toggleCompleted: function(position) {
+        var todo = this.todos[position];
+        todo.coompleted = !todo.completed;
+        this.displayTodos();
     }
-    
-function addTodo(todos) {
-	todos.push(todos);
-	displayTodos()
-    }
-    
-function changeTodo(position, newValue) {
-    todos[position] = newValue;
-	displayTodos();
-}
-
-function deleteTodo(position) {
-    todos.splice(position, 1)
-    displayTodos();
-}
