4d53154c10ee86975ef307480b3ad2da602d3fe5
bferrell514 <bferrell514@gmail.com>
2019-03-30 12:12:45 -0400

    adding for loops to my file

--- todoList.js
+++ todoList.js
@@ -1,25 +1,31 @@
 var todoList = {
     todos: [], 
     displayTodos: function() {
-        console.log('My Todos', this.todos)
-    },
+        if (this.todos.length === 0){
+            console.log('Your todo list is empty!')
+        } else {
+            for (var i = 0; i < this.todos.length; i++) {
+            
+            if (this.todos[i].completed === true) {
+                console.log('(x)', this.todos[i].todoText);
+            } else {
+                console.log('()', this.todos[i].todoText);
+            }
+        }
+    }
+},
     addTodo: function(todoText) {
         this.todos.push({
-            todoText: todoText,
-            completed: false,
-    });
-    this.displayTodos()
+           todoText: todoText,
+           completed: false
+        });
     },
-    changeTodo: function(position, todoText) {
-        this.todos[position].todoText = todoText;
+    changeTodo: function(position, newValue) {
+        this.todos[position] = newValue;
         this.displayTodos();
     },
     deleteTodo: function(position) {
         this.todos.splice(position, 1)
         this.displayTodos();
-    },
-    toggleCompleted: function(position) {
-        var todo = this.todos[position];
-        todo.coompleted = !todo.completed;
-        this.displayTodos();
     }
+};
